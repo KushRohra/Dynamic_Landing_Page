510190b67de8ada136c66b045221aa6f1d19f85e
KushRohra <iec2017024@iiita.ac.in>
2020-03-19 23:04:57 +0530

    name and focus functions added

--- js/main.js
+++ js/main.js
@@ -19,7 +19,7 @@ function showTime()
 	hour = hour % 12 || 12;
 
 	//Output the time
-	time.innerHTML = `${hour}<span>:</span>${addZeros(min)}<span>:</span>${addZeros(sec)}`;
+	time.innerHTML = `${hour}<span>:</span>${addZeros(min)}<span>:</span>${addZeros(sec)} ${ampm}`;
 
 	setTimeout(showTime, 1000);
 }
@@ -56,9 +56,75 @@ function setBgGreet()
 		greeting.textContent = 'Good Evening';
 		document.body.style.color = 'white';
 	}
-
 }
+
+//Get name
+function getName()
+{
+	if(localStorage.getItem('name') === null)
+	{
+		name.textContent = '[Enter Name]';
+	}
+	else
+	{
+		name.textContent = localStorage.getItem('name');
+	}
+}
+
+function setName(e)
+{
+	if(e.type === 'keypress')
+	{
+		//Make sure Enter is pressed
+		if(e.which == 13 || e.keyCode == 13)
+		{
+			localStorage.setItem('name', e.target.innerText);
+			name.blur();
+		}
+	}
+	else
+	{
+		localStorage.setItem('name', e.target.innerText);
+	}
+}
+
+//Get focus
+function getFocus()
+{
+	if(localStorage.getItem('focus') === null)
+	{
+		focus.textContent = '[Enter Focus]';
+	}
+	else
+	{
+		focus.textContent = localStorage.getItem('focus');
+	}
+}
+
+function setFocus(e)
+{
+	if(e.type === 'keypress')
+	{
+		//Make sure Enter is pressed
+		if(e.which == 13 || e.keyCode == 13)
+		{
+			localStorage.setItem('focus', e.target.innerText);
+			focus.blur();
+		}
+	}
+	else
+	{
+		localStorage.setItem('focus', e.target.innerText);
+	}
+}
+
+name.addEventListener('keypress', setName);
+name.addEventListener('blur', setName);
+focus.addEventListener('keypress', setFocus);
+focus.addEventListener('blur', setFocus);
 
 //Run
 showTime();
 setBgGreet();
+getName();
+getFocus();
